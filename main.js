9a36fde8e74492d2837d60d8a468ec17de620b9e
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-30 21:16:17 +0200

    aggiunta pallini

--- main.js
+++ main.js
@@ -3,20 +3,26 @@
 
 //funzione click su classe next
 $('.next').click(function() {
-    //recupero l'img che ha la classe active in questo momento  e la salvo in una variabile
+    //recupero l'img e il pallino che ha la classe active in questo momento  e la salvo in una variabile
     var imgCurrent = $('img.active');
-
-    //rimuovo la classe active all'immagine corrente
+    var currentBullet = $('i.active');
+    //rimuovo la classe active all'immagine corrente e al pallino
     imgCurrent.removeClass('active');
-    //recupero l'immagine succesiva
+    currentBullet.removeClass('active');
+    //recupero l'immagine succesiva e il pallino succ
     var imgNext = imgCurrent.next('img');
-    //verifico che esista un0mmagine successiva
+    var nextBullet = currentBullet.next('i');
+    //verifico che esista un0mmagine successiva eun pallino successivo
     if (imgNext.length != 0) {
+        //se esiste metto la classe active
         imgNext.addClass('active');
+        nextBullet.addClass('active');
     } else {
         //se non c'è riparto dall'inizio
         imgNext = $('img:first-child');
+        nextBullet = $('i:first-child');
         imgNext.addClass('active');
+        nextBullet.addClass('active');
     }
 
 
@@ -25,20 +31,25 @@ $('.next').click(function() {
 
 //procedimento inverso per freccette per tornare indietro
 $('.prev').click(function() {
-    //
+    //    //recupero l'img e il pallino che ha la classe active in questo momento  e la salvo in una variabile
     var imgCurrent = $('img.active');
-
-    //
+    var currentBullet = $('i.active');
+    ////rimuovo la classe active all'immagine corrente e al pallino
     imgCurrent.removeClass('active');
-    //
+    currentBullet.removeClass('active');
+    //    //recupero l'immagine precedente e il pallino precedente
     var imgPrev = imgCurrent.prev('img');
-    //
+    var prevBullet = currentBullet.prev('i');
+    //    //verifico che esista un0mmagine precedente  eun pallino precedente
     if (imgPrev.length != 0) {
         imgPrev.addClass('active');
+        prevBullet.addClass('active');
     } else {
-        //
-        imgPrev = $('img:last-child');
+        ////se non c'è riparto
+        imgPrev = $('img:last-of-type');
+        prevBullet = $('i:last-child');
         imgPrev.addClass('active');
+        prevBullet.addClass('active');
     }
 
 
